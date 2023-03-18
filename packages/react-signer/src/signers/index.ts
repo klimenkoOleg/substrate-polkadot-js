65d8fe5d4f5bee24f41d9b8d570da86d221841a5
Oleg_Klimenko <oleg_klimenko@epam.com>
2023-03-18 16:43:16 -0700

    prune old version

--- packages/react-signer/src/signers/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2017-2023 @polkadot/react-signer authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export { default as AccountSigner } from './AccountSigner.js';
-export { default as ApiSigner } from './ApiSigner.js';
-export { default as LedgerSigner } from './LedgerSigner.js';
-export { default as QrSigner } from './QrSigner.js';
